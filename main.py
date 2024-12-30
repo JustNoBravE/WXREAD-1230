d6ef0002d1552e8b874b0b93d0c0629ce4fb2940
Brave <sedonadeduction@gmail.com>
2024-12-30 15:34:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import logging
 import hashlib
 import requests
 import urllib.parse
-from push import push
-from config import data, headers, cookies, READ_NUM, PUSH_METHOD
+from config import data, headers, cookies, READ_NUM,
 
 # 配置日志格式
 logger = logging.getLogger(__name__)
